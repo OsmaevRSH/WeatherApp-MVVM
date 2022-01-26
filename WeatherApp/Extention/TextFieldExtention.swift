bfb32d02c46815b35fcf2edaced1776834b9ba31
Osmaev Ruslan <osmaevrsh@gmail.com>
2022-01-26 17:32:30 +0300

    renamed project and added appicon

---
+++ WeatherApp/Extention/TextFieldExtention.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldExtention.swift
+//  WeatherApp
+//
+//  Created by Руслан Осмаев on 25.01.2022.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+extension UITextField {
+	var textPublisher: AnyPublisher<String, Never> {
+		NotificationCenter
+			.default
+			.publisher(for: UITextField.textDidChangeNotification, object: self)
+			.compactMap {($0.object as? UITextField)?.text}
+			.eraseToAnyPublisher()
+	}
+}
---
+++ WeatherApp/ViewModel/ViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ViewModel.swift
+//  WeatherApp
+//
+//  Created by Руслан Осмаев on 25.01.2022.
+//
+
+import Foundation
+import Combine
+
+class ViewModel {
+	
+	@Published var city = "London"
+	@Published var weather = WeatherModel.placeholder
+	
+	private var cancelableSet: Set<AnyCancellable> = []
+	
+	init() {
+		$city
+			.debounce(for: .milliseconds(500), scheduler: RunLoop.main, options: nil)
+			.removeDuplicates()
+			.flatMap {
+				(city: String) -> AnyPublisher<WeatherModel, Never> in
+				WeatherAPI.shared.getWeather(city: city)
+			}
+			.assign(to: \.weather, on: self)
+			.store(in: &cancelableSet)
+	}
+}
